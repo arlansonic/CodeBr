ffc7c4ab9eceb817e0440f50756a8546fdba0d77
Arlan <arlan_marreiro@cal-com.com.br>
2021-01-27 17:14:39 -0400

    Push Exercises 32 to 37

--- exercicios-js/exercicios/Exercicio-31.js
+++ exercicios-js/exercicios/Exercicio-31.js
@@ -13,6 +13,6 @@ function numNegativos(vetor){
     }
     console.log(`Quantidade de numeros negativos ${qtdNegativos}`)
 }
-vetor = [ 7, 8, 9, -10, 11, 12, -16, -17, 18, 19, 20, 21]
+vetor = [ 7, 8, 9, -10, 11, -12, -16, -17, 18, 19, 20, 21]
 
 numNegativos(vetor)
