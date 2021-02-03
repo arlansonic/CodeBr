a44102241da346f262e10792b232d6119fd72cec
Arlan <arlan_marreiro@cal-com.com.br>
2021-02-03 17:00:33 -0400

    Update - Add: Object.values or Object.keys and Object.entries

--- exercicios-js/exercicios/ExercicioPraticos.js
+++ exercicios-js/exercicios/ExercicioPraticos.js
@@ -51,6 +51,21 @@ function puxarObjeto(){
         }
       }
     }
+console.log('Unidades Calcomp: '+Object.keys(clientesProducao))
+// console.log(sistemaProducao)    
+console.log('Bancos CS: '+ Object.keys(clientesProducao.CS))
+console.log('Clientes Locados no SGDV: '+Object.values(clientesProducao.CS.SGDV))
+console.log('Clientes Locados no CSMB: ' + Object.values(clientesProducao.CS.CSMB))
+console.log('--------------------------------')
 
-    console.log(clientesProducao.CS)
-    
+console.log('Bancos CR: ' + Object.keys(clientesProducao.CR))
+console.log('Clientes Locados TSDB: ' + Object.values(clientesProducao.CR.TSDB))
+console.log('Clientes Locados PBDB: ' + Object.values(clientesProducao.CR.PBDB))
+console.log('Clientes Locados no PADB: ' + Object.values(clientesProducao.CR.PADB))
+
+console.log('-----------Object.entries------------')
+console.log('Bancos CS - SGDV: ' + Object.entries(clientesProducao.CS.SGDV))
+console.log('Bancos CS - CSMB: ' + Object.entries(clientesProducao.CS.CSMB))
+Object.entries(clientesProducao.CS.SGDV).forEach(e =>{
+    console.log(`${[0]}: ${[1]}`)
+})
