ac03e2be542fe08357a0290271251b2d06a28830
Arlan <arlan_marreiro@cal-com.com.br>
2021-03-02 02:21:38 -0400

    Creating por 3003 and using GET and POST - Express

--- exercicios-js/node/projeto/src/server.js
+++ exercicios-js/node/projeto/src/server.js
@@ -2,9 +2,23 @@ const porta = 3003
 
 const express = require('express')
 const app = express('app')
+const BancoDeDados = require('./bancoDeDados')
 
 app.get('/produtos',(req, res, next)=>{
-    res.send({nome: 'Notebook', preco: 2000}) // Converter para formato JSON
+    res.send(BancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id',(req, res, next) =>{
+    res.send(BancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos',  (res, req, next)=>{
+    const produto = BancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+
+    res.send(produto) //Retornar JSON
 })
 
 app.listen(porta, () =>{
