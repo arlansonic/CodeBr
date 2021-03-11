5b774093f67d2b05b77126bb3b383d8e85d39ca5
Arlan <arlan_marreiro@cal-com.com.br>
2021-03-11 00:35:39 -0400

    Create Form + Node + Express

---
+++ exercicios-js/html/exercicios/form/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const badyParser = require('body-parser')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post('/usuarios', (req, resp) =>{
+    console.log(req.body)
+    resp.send('<h3>Incluido com Sucesso!!</h3>')
+})
+
+app.listen(3003)
