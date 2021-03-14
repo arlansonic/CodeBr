0812f155b799225a50f135107f7ea0b07005257f
Arlan <arlan_marreiro@cal-com.com.br>
2021-03-13 23:44:43 -0400

    Update ClasseTag

--- exercicios-js/css/js/tag.js
+++ exercicios-js/css/js/tag.js
@@ -1,4 +1,15 @@
-document.querySelectorAll('.tag').foreach(box=>{
-    
-})
+document.querySelectorAll('.tag').forEach(elemento => {
+    const tagName = elemento.tagName.toLowerCase()
 
+
+    elemento.style.borderColor = '#616161'
+
+        if(!elemento.classList.contains('nolabel')){
+            const label = document.createElement('label')
+            label.style.backgroundColor = '#616161'
+            label.innerHTML = tagName
+            elemento.insertBefore(label, elemento.childNodes[0])
+
+        }
+
+    })
