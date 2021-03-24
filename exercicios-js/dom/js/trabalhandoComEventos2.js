f933523f79d43270874ace2110ea5cc85de7e6bd
Arlan <arlan_marreiro@cal-com.com.br>
2021-03-23 22:51:09 -0400

    Working with events - Divs within the other - TrabalhandoComEventos3

--- exercicios-js/dom/js/trabalhandoComEventos2.js
+++ exercicios-js/dom/js/trabalhandoComEventos2.js
@@ -1,11 +1,28 @@
-const item = document.querySelector('.elemento')
+// const item = document.querySelector('.elemento')
+// item.style.position = 'absolute'
+
+// item.onmousemove = e =>{
+//     const item = e.target
+//     item.style.cursor = 'move'
+//     if(e.buttons){
+//         item.style.top = `${e.clientY - (item.clientHeight / 2)}px`
+//         item.style.left = `${e.clientX - (item.clientWidth / 2)}px`
+//     }
+// }
+
+const item = document.querySelector('.elemento1')
 item.style.position = 'absolute'
 
 item.onmousemove = e =>{
-    const item = e.target
+    const item = e.target    
     item.style.cursor = 'move'
+    
     if(e.buttons){
-        item.style.top = `${e.clientY - (item.clientHeight / 2)}px`
-        item.style.left = `${e.clientX - (item.clientWidth / 2)}px`
+        item.style.top = `${e.clientY - (item.clientHeight /2)}px`
+        item.style.left = `${e.clientX - (item.clientWidth /2)}px`
     }
 }
+
+const item2 = document.querySelector('.elemento2')
+item2.style.display = 'flex'
+item2.style.margin = '10%'
---
+++ exercicios-js/dom/js/trabalhandoComEventos3.js
@@ -0,0 +1,18 @@
+const items = document.querySelectorAll('[wm-dropzone] .item')
+items.forEach((item, index) => {
+    item.draggable = true
+    item.id = item.id || `draggable-item-${index}`
+    item.ondragstart = e =>
+        e.dataTransfer.setData('item-id', e.target.id)
+})
+
+const dropzones = document.querySelectorAll('[wm-dropzone]')
+dropzones.forEach(dropzone => {
+    dropzone.ondragover = e => e.preventDefault()
+    dropzone.ondrop = function(e){
+        const id = e.dataTransfer.getData('item-id')
+        const item = document.getElementById(id)
+        e.target.appendChild(item) // Adicionar Divs um dentro do outro
+        // dropzone.appendChild(item) // Não deixar as Divs entrarem uma dentro da outra
+    }
+})
