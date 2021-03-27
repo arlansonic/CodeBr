e5355b4a6dd3b1165f04f46d5cb3383a74eaa49b
Arlan <arlan_marreiro@cal-com.com.br>
2021-03-26 21:36:56 -0400

    Getting an Ajax request from an XML

---
+++ ajax/js/xmlHttpRequest3.js
@@ -0,0 +1,35 @@
+function ajax(config){
+    const xhr = new XMLHttpRequest()
+    xhr.open(config.metodo, config.url, true)
+
+    // Requisição para XML
+    xhr.onreadystatechange = e => {
+        if(xhr.readyState === 4){
+            config.sucesso(xhr.responseXML)
+        }else if(xhr. status >= 400){
+            config.erro({
+                codigo: xhr.status,
+                texto: xhr.statusText
+            })
+        }
+    }
+    // Enviar 
+    xhr.send()
+}
+
+
+ajax({
+    url: "dados/estados.xml",
+    metodo: "get",
+    sucesso(resposta){
+        const estados = resposta.getElementsByTagName('estado')
+        const itens = Array.from(estados).reduce(
+            (html, estado) => html + `<li>${estado.attributes.nome.value}</li>`, ''
+        )
+        document.body.insertAdjacentHTML('beforeend', `<ul> ${itens} </ul>`)
+    },
+    erro(e){
+        const msg = document.createTextNode(`${e.codigo}: ${e.texto}`)
+        document.body.appendChild(msg)
+    }
+})
