93aebc3e0fdfdd662340aa03bddf12e395e6c2c9
Arlan <arlan_marreiro@cal-com.com.br>
2021-03-29 21:36:51 -0400

    Update Fetch1

--- ajax/js/fetch1.js
+++ ajax/js/fetch1.js
@@ -1,9 +1,10 @@
 const url ='dados/estados.json'
+
 fetch(url)
     .then(resp => resp.json())
     .then(estados =>{
         const itens = estados.reduce(
-            (html, estado) => html + `<li>${estado.nome}</li>`
+            (html, estado) => html + `<li> ${estado.id} - ${estado.nome} - ${estado.sigla}</li>`
         )
         document.body.insertAdjacentHTML('beforeend', `<ul>${itens}</ul>`)
     })
