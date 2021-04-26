b58d342fe4a9852dd0253b29b7746b03062b8e14
Arlan <arlan_marreiro@cal-com.com.br>
2021-04-26 18:40:33 -0400

    Create Frist Component React

---
+++ React/exercicios/src/componentes/Primeiro.jsx
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export default () => <h1>Arlan Marreiro de Souza Nascimento</h1>
--- React/exercicios/src/index.js
+++ React/exercicios/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import Primeiro from './componentes/Primeiro'
 
-const elemento = <h1>React</h1>
-const elem = <h1>React 02</h1>
+// const elemento = <h1>React</h1>
+// const elem = <h1>React 02</h1>
 
-ReactDom.render(elemento, document.getElementById('root'))
+ReactDom.render(<Primeiro/>, document.getElementById('root'))
 
-ReactDom.render(elem, document.getElementById('root2'))
