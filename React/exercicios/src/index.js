634b0f9770996176a34633db0c1f3058605bfcdd
Arlan <arlan_marreiro@cal-com.com.br>
2021-04-26 23:21:43 -0400

    Class Components

--- React/exercicios/src/index.js
+++ React/exercicios/src/index.js
@@ -5,11 +5,14 @@ import BomDia from './componentes/BomDia'
 import MinisterioDaFazenda from './componentes/MinisterioDaFazenda'
 import { BoaNoite, BoaTarde } from './componentes/Multiplos' //Forma 1 de Exportar
 import Multi from './componentes/Multiplos' //Forma 2 de Exportar
+import Saudacao from './componentes/Saudacao'
 
 // const elemento = <h1>React</h1>
 // const elem = <h1>React 02</h1>
 
 // ReactDom.render(<Primeiro/>, document.getElementById('root'))
+
+/**
 ReactDom.render(<BomDia nome="Arlan Marreiro" idade={26}/>, document.getElementById('root'))
 ReactDom.render(<MinisterioDaFazenda group="Ministério da Fazenda" membros={10}
 comuns={2} adms={8}
@@ -31,4 +34,12 @@ ReactDom.render(
         <Multi.BoaNoite nome="Apollo Henrique"/>
     </div>
 
+,document.getElementById('root'))
+
+ */
+
+ReactDom.render(
+    <div>
+        <Saudacao tipo="Bom Dia" nome="Arlan Marreiro"/>
+    </div>
 ,document.getElementById('root'))
