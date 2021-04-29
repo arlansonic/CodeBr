529bbf7be4f426eaff0ff25def7bb9784ee871f1
Arlan <arlan_marreiro@cal-com.com.br>
2021-04-28 21:46:31 -0400

    Components Buttons 2 - Calculator

--- React/calculadora/src/components/Button.jsx
+++ React/calculadora/src/components/Button.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import './Button.css'
 
-export default props =>
+export default props => {
+    let classes = 'button '
 
-<button className="button">{props.label}</button>
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+    return (
+        <button
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>
+    )
+}
