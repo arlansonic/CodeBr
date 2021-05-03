f5cd753af5169866a33a3a2c76f44780743569a5
Arlan <arlan_marreiro@cal-com.com.br>
2021-05-03 19:50:59 -0400

    Applying styles with Css Grid

--- React/crud/frontend/src/main/App.jsx
+++ React/crud/frontend/src/main/App.jsx
@@ -5,11 +5,13 @@ import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
 import Main from '../components/template/Main'
 import Footer from '../components/template/Footer'
+import Header from '../components/template/Header'
 
 export default props =>
     <div className="app">
         <Logo />
         <Nav />
+        <Header/>
         <Main />
         <Footer />
     </div>
