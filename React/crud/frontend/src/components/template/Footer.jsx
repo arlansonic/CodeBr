c21d1a8830064e65d3971b35ed7b46f0074a79f8
Arlan <arlan_marreiro@cal-com.com.br>
2021-05-03 23:32:41 -0400

    Component Footer

--- React/crud/frontend/src/components/template/Footer.jsx
+++ React/crud/frontend/src/components/template/Footer.jsx
@@ -4,5 +4,8 @@ import React from 'react'
 export default props =>
 
     <footer className="footer">
-        Rodapé
-</footer>
+        <span>
+            Desenvolvido com  <i class="fa fa-heart text-danger"></i> por
+            <strong><span className="text-danger">A</span> rlan <span className="text-danger">M</span>arreiro</strong>
+        </span>
+    </footer>
