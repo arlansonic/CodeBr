9ddf80af4fbcd74b963dcc25682f29dbde273ce8
Arlan <arlan_marreiro@cal-com.com.br>
2021-05-04 00:40:19 -0400

    Component Nav

--- React/crud/frontend/src/components/template/Footer.jsx
+++ React/crud/frontend/src/components/template/Footer.jsx
@@ -6,6 +6,6 @@ export default props =>
     <footer className="footer">
         <span>
             Desenvolvido com  <i class="fa fa-heart text-danger"></i> por
-            <strong><span className="text-danger">A</span> rlan <span className="text-danger">M</span>arreiro</strong>
+            <strong><span className="text-danger">A</span>rlan <span className="text-danger">M</span>arreiro</strong>
         </span>
     </footer>
--- React/crud/frontend/src/components/template/Nav.jsx
+++ React/crud/frontend/src/components/template/Nav.jsx
@@ -3,5 +3,12 @@ import React from 'react'
 
 export default props =>
     <aside className="menu-area">
-        Navegação
-</aside>
+        <nav className="menu">
+        <a href="#">
+            <i className="fa fa-home"></i> Home
+        </a>
+        <a href="#/users">
+            <i className="fa fa-users"></i> Usuários
+        </a>
+        </nav>
+    </aside>
