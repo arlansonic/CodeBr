a7d8fb99a4e008cdfb52afde3ac597a3b48dc534
Arlan <arlan_marreiro@cal-com.com.br>
2021-05-07 23:19:46 -0400

    Template at Vue Instance

---
+++ vue/exercicios/js/template.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: "#app",
+    data: {
+        titulo: "VueJS",
+        subtitulo: "Framework Progressivo"
+    },
+    template: `
+    <div>
+        <h1>{{titulo}}</h1>
+        <h3>{{subtitulo}}</h3>
+    </div>
+    `
+
+
+})
