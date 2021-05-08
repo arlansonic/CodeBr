8a715e1e6e43e32e1e5af6d32bd18c8afd242610
Arlan <arlan_marreiro@cal-com.com.br>
2021-05-07 22:52:51 -0400

    Directives - v - if | v - for | v - show

---
+++ vue/exercicios/js/ifFor.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: "#app",
+    data: {
+        alunos: [
+            { nome: "Arlan Marreiro", nota: 8.0 },
+            { nome: "Maria Katiane", nota: 7.5 },
+            { nome: "Apollo Henrique", nota: 10 },
+            { nome: "Hero", nota: 3 },
+            { nome: "Arlison", nota: 2.5 }
+        ]
+    }
+})
