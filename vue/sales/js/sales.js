90d3e2f6fbad47a1187739e0d1d4d87901b9b31d
Arlan <arlan_marreiro@cal-com.com.br>
2021-05-12 21:13:26 -0400

    Sales Summary Project

--- vue/sales/js/sales.js
+++ vue/sales/js/sales.js
@@ -4,7 +4,7 @@ Vue.directive('money', {
         const amount = parseFloat(el.innerHTML).toFixed(2).replace('.', ',')
             .replace(/(\d)(?=(\d{3})+\,)/g, '$1.')
 
-            el.innerHTML = `${binding.value} ${amount}`
+        el.innerHTML = `${binding.value} ${amount}`
     }
 })
 
@@ -19,6 +19,32 @@ new Vue({
             fetch('/data/sales.json') // pegar os arquivos Json
                 .then(resp => resp.json()) // retornar o JSON
                 .then(data => this.sales = data) //console.log(data) - para checar se ta puxando
+        },
+
+        calculateTotal(sales) {
+            if (!sales) return 0
+            return sales.reduce((total, sale) => total + sale.value, 0)
+        }
+    },
+    computed: {
+        salesByProduct() {
+            if (!this.sales) return null
+
+            const salesByProduct = this.sales.reduce((grouped, sale) => {
+                if (!grouped.hasOwnProperty(sale.name)) {
+                    grouped[sale.name] = {
+                        name: sale.name,
+                        value: sale.price * sale.qtde
+                    }
+
+                    return grouped
+                }
+
+                grouped[sale.name].value += sale.price * sale.qtde
+                return grouped
+            }, {})
+
+            return Object.values(salesByProduct)
         }
     },
     mounted() {
