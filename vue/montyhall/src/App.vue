d6cf0c68b6c44ab200e7167f8a2f5a18bf815419
Arlan <arlan_marreiro@cal-com.com.br>
2021-05-18 22:36:58 -0400

    Project - Vue: MontyHall

---
+++ vue/montyhall/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+      <h1>Problema de Monty Hall</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+*{
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Montserrat';
+}
+
+body{
+    color: white;
+    background: #000428;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #004e92, #000428);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #004e92, #000428); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    
+}
+
+#app{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+#app h1{
+    border: 1px solid #000;
+    background-color: #004;
+    padding: 20px;    
+    margin-top: 20px;
+}
+</style>
