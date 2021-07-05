22862d2107ec3e36b5cc5af0fd0468cf0a896ac1
Arlan <arlan_marreiro@cal-com.com.br>
2021-07-05 19:48:23 -0400

    Create Function Methods HTTP: app.use

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.use((req, res) => {
+    res.send('Funcionou meu considerado: Hehueheuheuhehuu')
+})
+
+
 app.listen(3000, () => {
     console.log('Executando serviço')
 })
