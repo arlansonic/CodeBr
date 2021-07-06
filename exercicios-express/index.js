738bccb89e6e1487e3747458663ee5f646a3c795
Arlan <arlan_marreiro@cal-com.com.br>
2021-07-05 22:32:53 -0400

    Middleware Next

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,8 +1,40 @@
 const express = require('express')
 const app = express()
 
-app.use((req, res) => {
-    res.send('Funcionou meu considerado: Hehueheuheuhehuu')
+
+app.use('/api', (req, res, next) => {
+    console.log('Antes...')
+    next()
+})
+
+app.get('/api', (req, res, next) => {
+    console.log('Durante.....')
+        // res.send('<h1>Funcionou meu considerado:</h1> <h2><b>Hehueheuheuhehuu</b></h2>')
+
+    // res.json({
+    //     name: 'Ipad 32GB',
+    //     price: 2000.00,
+    //     discount: 0.12
+    // })
+
+    res.json({
+        data: [
+            { id: 300, name: 'Arlan Marreiro', idade: 27, position: 1 },
+            { id: 500, name: 'Maria Katiane', idade: 29, position: 2 },
+            { id: 600, name: 'Apollo Henrique', idade: 7, position: 3 }
+        ],
+        count: 30,
+        skip: 0,
+        limit: 3,
+        status: 200
+    })
+
+    next()
+})
+
+
+app.use('/api', (req, res) => {
+    console.log('Depois......')
 })
 
 
